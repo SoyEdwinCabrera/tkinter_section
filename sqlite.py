bd4e06a07dcb196eee720639f5c5067539944a10
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-07-08 11:22:31 -0500

    Update(sqlite.py): Selecting, Inserting, Deleting and Updating SQL Records

--- sqlite.py
+++ sqlite.py
@@ -1,27 +1,44 @@
 import sqlite3
 
 def create_table():
-    conn=sqlite3.connect("lite.db")
-    cur=conn.cursor()
-    cur.execute("CREATE TABLE store (item TEXT, quantity INTEGER, price REAL)")
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 def insert(item, quantity, price):
-    conn=sqlite3.connect("lite.db")
-    cur=conn.cursor()
-    cur.execute("INSERT INTO store VALUES(?,?,?)",(item, quantity, price))
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?, ?, ?)", (item, quantity, price))
     conn.commit()
     conn.close()
 
-insert("Coffe Cup",8,10.5)
-
 def view():
-    conn=sqlite3.connect("lite.db")
-    cur=conn.cursor()
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
     cur.execute("SELECT * FROM store")
-    rows=cur.fetchall()
+    rows = cur.fetchall()
     conn.close()
     return rows
+    
+def delete(item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?", (item))
+    conn.commit()
+    conn.close()
+    
+def update(quantity,price,item):
+    conn =sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity, price, item))
+    conn.commit()
+    conn.close()
 
+# Crear la tabla antes de insertar datos
+create_table()
+# insert("Water Glass",10,5)
+# delete("Watter Glass")
+update(11, 6, "Water Glass")
 print(view())
